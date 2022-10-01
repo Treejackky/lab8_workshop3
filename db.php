50f37b3e9aec048629a8bc092aea3c6e2ef61d19
Tontree <70340189+Treejackky@users.noreply.github.com>
2022-10-01 09:56:17 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,29 @@
+<?php include "connect.php";?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$stmt = $pdo->prepare("SELECT * FROM member product");
+
+
+$stmt->execute();
+
+$x = 1;
+while ($row = $stmt->fetch()) { 
+echo "ชื่อสมาชิก: ".$row["name"]."<br>";
+echo "ที่อยู่: ".$row["address"]."<br>";
+echo "อีเมลล์: ".$row["email"]."<br>";
+echo "<img src = 'img/$x.jpg'"."<br>";
+$x++;
+echo "<br>";
+}
+?>
+</body>
+</html>
